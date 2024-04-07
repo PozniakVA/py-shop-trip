5cd88b49498dd9e6c49c054f20b61ec18c008d1a
Valentin Pozniak <waliker448@gmail.com>
2024-04-07 21:27:15 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,51 @@
-def shop_trip():
-    # write your code here
-    pass
+import json
+
+from app.customer import Customer
+from app.shop import Shop, find_trip_cost, shopping
+
+
+def shop_trip() -> None:
+    with open("app/config.json", "r") as data:
+        total_data = json.load(data)
+
+    for customer_data in total_data["customers"]:
+        customer = Customer(*customer_data.values())
+        print(f"{customer.name} has {customer.money} dollars")
+
+        shops = {}
+        for shop_data in total_data["shops"]:
+            shop = Shop(*shop_data.values())
+            trip_cost = find_trip_cost(
+                customer.location,
+                shop.location,
+                total_data["FUEL_PRICE"],
+                customer.car["fuel_consumption"]
+            )
+            total_costs = (
+                trip_cost
+                + sum(value * shop.products[key]
+                      for key, value in customer.product_cart.items())
+            )
+            print(
+                f"{customer.name}'s trip to the {shop.name} "
+                f"costs {total_costs}"
+            )
+            shops[total_costs] = shop
+
+        the_nearest_shop = shops[min(shops)]
+        if customer.money < min(shops):
+            return print(
+                f"{customer.name} doesn't have enough "
+                f"money to make a purchase in any shop"
+            )
+        print(f"{customer.name} rides to {the_nearest_shop.name}\n")
+
+        shopping(
+            customer.name,
+            customer.product_cart,
+            the_nearest_shop.products
+        )
+
+        print(
+            f"{customer.name} rides home\n"
+            f"{customer.name} now has {customer.money - min(shops)} dollars\n")
---
+++ app/shop.py
@@ -0,0 +1,53 @@
+import math
+from datetime import datetime
+
+
+class Shop:
+    def __init__(
+            self,
+            name: str,
+            location: list,
+            products: dict
+    ) -> None:
+        self.name = name
+        self.location = location
+        self.products = products
+
+
+def shopping(
+        name: str,
+        products: dict,
+        prices: dict
+) -> None:
+    print(
+        datetime.now().strftime("Date: %d/%m/%Y %H:%M:%S\n"),
+        f"Thanks, {name}, for your purchase!\n"
+        f"You have bought: "
+    )
+    total_cost = 0
+    for product in products:
+        total_sum_for_product = prices[product] * products[product]
+        if total_sum_for_product == int(total_sum_for_product):
+            total_sum_for_product = int(total_sum_for_product)
+        print(
+            f"{products[product]} {product}s "
+            f"for {total_sum_for_product} dollars"
+        )
+        total_cost += total_sum_for_product
+
+    print(
+        f"Total cost is {total_cost} dollars\n"
+        f"See you again!\n"
+    )
+
+
+def find_trip_cost(
+        customer_location: list,
+        shop_location: list,
+        fuel_price: int | float,
+        fuel_consumption: int | float
+
+) -> float | int:
+    distance = math.dist(customer_location, shop_location)
+    trip_cost = (distance * 2 * fuel_consumption * fuel_price) / 100
+    return round(trip_cost, 2)
